69d662b6a0f441a4ed3520c7cc081d516de2c396
bprinty <bprinty@gmail.com>
2020-03-18 08:08:31 -0500

    Working through error testing

---
+++ tests/errors.test.js
@@ -0,0 +1,54 @@
+/**
+ * Testing for package.
+ */
+
+
+// imports
+// -------
+import axios from 'axios';
+import { assert } from 'chai';
+import server from './server';
+
+
+// config
+// ------
+jest.mock('axios');
+server.init();
+beforeEach(() => {
+  server.reset();
+});
+
+
+// tests
+// -----
+describe('errors', () => {
+  let res;
+
+  test('errors.notfound', async () => {
+    try {
+      await axios.get('/errors/missing');
+      assert.fail('Request should have thrown an error.');
+    } catch (err) {
+      assert.equal(err.status, 404);
+    }
+  });
+
+  test('errors.forbidden', async () => {
+    try {
+      await axios.get('/errors/forbidden');
+      assert.fail('Request should have thrown an error.');
+    } catch (err) {
+      assert.equal(err.status, 403);
+    }
+  });
+
+  test('errors.inernal', async () => {
+    try {
+      await axios.get('/errors/server');
+      assert.fail('Request should have thrown an error.');
+    } catch (err) {
+      assert.equal(err.status, 500);
+    }
+  });
+
+});

ba074c26401d9baf21c69985becf0094428d40dc
bprinty <bprinty@gmail.com>
2020-03-18 08:08:13 -0500

    Added singleton helper tests

---
+++ tests/singleton.test.js
@@ -0,0 +1,55 @@
+/**
+ * Testing for package.
+ */
+
+
+// imports
+// -------
+import axios from 'axios';
+import { assert } from 'chai';
+import server from './server';
+
+
+// config
+// ------
+jest.mock('axios');
+server.init();
+beforeEach(() => {
+  server.reset();
+});
+
+
+// tests
+// -----
+describe('singleton', () => {
+  let res;
+
+  test('singleton.get', async () => {
+    res = await axios.get('/profile');
+    assert.equal(res.status, 200);
+    assert.deepEqual(res.data, { username: 'admin' });
+  });
+
+  test('singleton.put', async () => {
+    // udpate
+    res = await axios.put('/profile', { username: 'test' });
+    assert.equal(res.status, 200);
+    assert.deepEqual(res.data, { username: 'test' });
+
+    // check
+    res = await axios.get('/profile');
+    assert.deepEqual(res.data, { username: 'test' });
+  });
+
+  test('singleton.delete', async () => {
+    // update and delete
+    await axios.put('/profile', { username: 'test' });
+    res = await axios.delete('/profile');
+    assert.equal(res.status, 204);
+
+    // check
+    res = await axios.get('/profile');
+    assert.deepEqual(res.data, { username: 'admin' });
+  });
+
+});
